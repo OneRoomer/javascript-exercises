1331ce03b7fd0cad7052aaea57009391c1a2e595
OneRoomer <rene.elfinger@mailbox.org>
2023-01-29 12:37:07 +0100

    Exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+var concatString = '';
 
+if (num < 0) {
+return "ERROR";
+}
+
+else {
+for (let i = 0; i < num; i++) {
+    concatString += string
+}
+return concatString;
+}
 };
 
 // Do not edit below this line
