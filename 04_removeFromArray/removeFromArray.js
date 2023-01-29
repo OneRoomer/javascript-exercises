49e81158d452b40384845fd374083d38d91c4674
OneRoomer <rene.elfinger@mailbox.org>
2023-01-29 14:19:40 +0100

    Exercise 4 (learned about args afterwards)

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,34 @@
-const removeFromArray = function() {
+const removeFromArray = function(myArray, outsider_1, outsider_2, outsider_3, outsider_4) {
+
+    const myHelpArray = Array.from(myArray);
+
+    for (var i = 0; i <= myArray.length; i++){
+        if (myHelpArray[i] === outsider_1){
+            myHelpArray.splice(i, 1);
+        }
+    }
+
+    for (var i = 0; i <= myArray.length; i++){
+
+        if (myHelpArray[i] === outsider_2) {
+            myHelpArray.splice(i, 1);
+        }
+    }
+
+    for (var i = 0; i <= myArray.length; i++){
+
+        if (myHelpArray[i] === outsider_3) {
+            myHelpArray.splice(i, 1);
+        }
+    }
+
+    for (var i = 0; i <= myArray.length; i++){
+
+        if (myHelpArray[i] === outsider_4) {
+            myHelpArray.splice(i, 1);
+        }
+    }
+    return myHelpArray;
 
 };
 
