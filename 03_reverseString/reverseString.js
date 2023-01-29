720c0a90162743c3d51e4a3df20f0c53b29493bd
OneRoomer <rene.elfinger@mailbox.org>
2023-01-29 13:10:24 +0100

    Exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(string) {
 
+    const myArray = string.split("");
+    var reverseString ="";
+
+    //Reverse the split String
+    for (var i = 1; i <= myArray.length; i++) {
+        reverseString += myArray[myArray.length-i];
+    }
+    return reverseString;
 };
 
 // Do not edit below this line
