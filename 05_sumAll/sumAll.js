a5121bb82978d9aaeef762a057dfeccb11ad644f
OneRoomer <rene.elfinger@mailbox.org>
2023-01-30 07:39:38 +0100

    Exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,33 @@
-const sumAll = function() {
+const sumAll = function(number_1, number_2) {
+
+    var sum = 0;
+
+    if (number_1 > 0 && number_2 > 0 && typeof(number_1) === "number" && typeof(number_2) === "number") {
+
+    if (number_1 > number_2) {
+        for(var i = number_2; i <= number_1; i++)
+        {
+            sum += i;
+        }
+    }
+    else if (number_1 < number_2){
+        for (var i = number_1; i <= number_2; i++) {
+            sum += i;
+        }
+    }
+
+    else {
+
+    }
+
+}
+
+else {
+    return 'ERROR';
+}
+    
+return sum;
+    
 
 };
 
