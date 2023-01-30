b203d306c2c38a9040aa82b27288cbe50450d597
OneRoomer <rene.elfinger@mailbox.org>
2023-01-30 08:24:53 +0100

    Exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,22 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farenheit) {
+
+  var toCelsius = 0;
+
+  toCelsius = (farenheit-32)*5/9;
+
+  
+  return Math.round(toCelsius * 10) / 10;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+
+  var toFarenheit = 0;
+
+  toFarenheit = (celsius * 9/5) + 32;
+
+
+  return Math.round(toFarenheit * 10) / 10;
 };
 
 // Do not edit below this line
